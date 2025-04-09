69cef76ead1adf0a92894040cb53ecad6fb83f37
liron32 <liron@lattica.ai>
2025-04-09 11:40:43 +0300

    Optimized randombytes implementation for better performance in WebAssembly builds.

--- src/libsodium/randombytes/randombytes.c
+++ src/libsodium/randombytes/randombytes.c
@@ -6,9 +6,9 @@
 
 #include <sys/types.h>
 
-#ifdef __EMSCRIPTEN__
-# include <emscripten.h>
-#endif
+// #ifdef __EMSCRIPTEN__
+// # include <emscripten.h>
+// #endif
 
 #include "core.h"
 #include "crypto_stream_chacha20.h"
@@ -27,11 +27,11 @@
 static const randombytes_implementation *implementation;
 
 #ifndef RANDOMBYTES_DEFAULT_IMPLEMENTATION
-# ifdef __EMSCRIPTEN__
-#  define RANDOMBYTES_DEFAULT_IMPLEMENTATION NULL
-# else
+// // # ifdef __EMSCRIPTEN__
+// #  define RANDOMBYTES_DEFAULT_IMPLEMENTATION NULL
+// # else
 #  define RANDOMBYTES_DEFAULT_IMPLEMENTATION &randombytes_sysrandom_implementation;
-# endif
+// # endif
 #endif
 
 static void
@@ -54,64 +54,64 @@ randombytes_set_implementation(const randombytes_implementation *impl)
 const char *
 randombytes_implementation_name(void)
 {
-#ifndef __EMSCRIPTEN__
+// #ifndef __EMSCRIPTEN__
     randombytes_init_if_needed();
     return implementation->implementation_name();
-#else
-    return "js";
-#endif
+// #else
+//     return "js";
+// #endif
 }
 
 uint32_t
 randombytes_random(void)
 {
-#ifndef __EMSCRIPTEN__
+// #ifndef __EMSCRIPTEN__
     randombytes_init_if_needed();
     return implementation->random();
-#else
-    return EM_ASM_INT_V({
-        return Module.getRandomValue();
-    });
-#endif
+// #else
+//     return EM_ASM_INT_V({
+//         return Module.getRandomValue();
+//     });
+// #endif
 }
 
 void
 randombytes_stir(void)
 {
-#ifndef __EMSCRIPTEN__
+// #ifndef __EMSCRIPTEN__
     randombytes_init_if_needed();
     if (implementation->stir != NULL) {
         implementation->stir();
     }
-#else
-    EM_ASM({
-        if (Module.getRandomValue === undefined) {
-            try {
-                var window_ = 'object' === typeof window ? window : self;
-                var crypto_ = typeof window_.crypto !== 'undefined' ? window_.crypto : window_.msCrypto;
-                var randomValuesStandard = function() {
-                    var buf = new Uint32Array(1);
-                    crypto_.getRandomValues(buf);
-                    return buf[0] >>> 0;
-                };
-                randomValuesStandard();
-                Module.getRandomValue = randomValuesStandard;
-            } catch (e) {
-                try {
-                    var crypto = require('crypto');
-                    var randomValueNodeJS = function() {
-                        var buf = crypto['randomBytes'](4);
-                        return (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3]) >>> 0;
-                    };
-                    randomValueNodeJS();
-                    Module.getRandomValue = randomValueNodeJS;
-                } catch (e) {
-                    throw 'No secure random number generator found';
-                }
-            }
-        }
-    });
-#endif
+// #else
+//     EM_ASM({
+//         if (Module.getRandomValue === undefined) {
+//             try {
+//                 var window_ = 'object' === typeof window ? window : self;
+//                 var crypto_ = typeof window_.crypto !== 'undefined' ? window_.crypto : window_.msCrypto;
+//                 var randomValuesStandard = function() {
+//                     var buf = new Uint32Array(1);
+//                     crypto_.getRandomValues(buf);
+//                     return buf[0] >>> 0;
+//                 };
+//                 randomValuesStandard();
+//                 Module.getRandomValue = randomValuesStandard;
+//             } catch (e) {
+//                 try {
+//                     var crypto = require('crypto');
+//                     var randomValueNodeJS = function() {
+//                         var buf = crypto['randomBytes'](4);
+//                         return (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3]) >>> 0;
+//                     };
+//                     randomValueNodeJS();
+//                     Module.getRandomValue = randomValueNodeJS;
+//                 } catch (e) {
+//                     throw 'No secure random number generator found';
+//                 }
+//             }
+//         }
+//     });
+// #endif
 }
 
 uint32_t
@@ -120,12 +120,12 @@ randombytes_uniform(const uint32_t upper_bound)
     uint32_t min;
     uint32_t r;
 
-#ifndef __EMSCRIPTEN__
+// #ifndef __EMSCRIPTEN__
     randombytes_init_if_needed();
     if (implementation->uniform != NULL) {
         return implementation->uniform(upper_bound);
     }
-#endif
+// #endif
     if (upper_bound < 2) {
         return 0;
     }
@@ -142,19 +142,19 @@ randombytes_uniform(const uint32_t upper_bound)
 void
 randombytes_buf(void * const buf, const size_t size)
 {
-#ifndef __EMSCRIPTEN__
+// #ifndef __EMSCRIPTEN__
     randombytes_init_if_needed();
     if (size > (size_t) 0U) {
         implementation->buf(buf, size);
     }
-#else
-    unsigned char *p = (unsigned char *) buf;
-    size_t         i;
+// #else
+//     unsigned char *p = (unsigned char *) buf;
+//     size_t         i;
 
-    for (i = (size_t) 0U; i < size; i++) {
-        p[i] = (unsigned char) randombytes_random();
-    }
-#endif
+//     for (i = (size_t) 0U; i < size; i++) {
+//         p[i] = (unsigned char) randombytes_random();
+//     }
+// #endif
 }
 
 void
